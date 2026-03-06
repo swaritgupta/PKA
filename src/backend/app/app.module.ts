5ca31d5e94296945ee89ab21f7f8bef170d260f4
Swarit Gupta <swaritgpt17@gmail.com>
2026-03-06 19:29:13 +0530

    Added & corrected dependencies

--- src/backend/app/app.module.ts
+++ src/backend/app/app.module.ts
@@ -7,6 +7,7 @@ import { DocumentModule } from '../document/document.module';
 import { ChatModule } from '../chat/chat.module';
 import { LlmModule } from '../llm/llm.module';
 import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UserModule } from '../user/user.module';
     ChatModule,
     LlmModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/backend/auth/auth.module.ts
+++ src/backend/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
