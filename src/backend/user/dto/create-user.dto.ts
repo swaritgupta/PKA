20d58ebedf3ba9ec4757ede7bd39a9b2b76e0504
Swarit Gupta <swaritgpt17@gmail.com>
2026-03-06 19:18:05 +0530

    Added health check to docker

--- src/backend/user/dto/create-user.dto.ts
+++ src/backend/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   username!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email!: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
   password!: string;
 }
